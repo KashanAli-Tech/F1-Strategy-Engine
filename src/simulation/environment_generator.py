aef5b3055f8dc6dbc0696bb1ffa121a83ea48a88
KashanAli-Tech <off.kashanali@gmail.com>
2026-07-26 13:40:37 +0100

    Improved race environment probability modelling

--- src/simulation/environment_generator.py
+++ src/simulation/environment_generator.py
@@ -5,39 +5,52 @@ from src.models.weather import Weather
 
 class EnvironmentGenerator:
 
-    def generate(self):
-        weather_roll = random.random()
+    WEATHER_PROBABILITIES = {Weather.DRY: 0.7,
+        Weather.LIGHT_RAIN: 0.25,
+        Weather.HEAVY_RAIN: 0.05}
 
-        if weather_roll < 0.7:
-            weather = Weather.DRY
 
-        elif weather_roll < 0.9:
-            weather = Weather.LIGHT_RAIN
+    def generate_weather(self):
+        roll = random.random()
+        cumulative = 0
 
-        else:
-            weather = Weather.HEAVY_RAIN
+        for weather, probability in self.WEATHER_PROBABILITIES.items():
+            cumulative += probability
 
-        safety_car_lap = None
+            if roll <= cumulative:
+                return weather
+
+    def generate_safety_car(self):
 
         if random.random() < 0.25:
-            safety_car_lap = random.randint(10, 45)
+            return random.randint(10, 45)
 
-        weather_change_lap = None
-        new_weather = None
+        return None
 
-        if random.random() < 0.30:
-            weather_change_lap = random.randint(15, 45)
+    def generate_weather_change(self, weather):
 
-            if weather == Weather.DRY:
-                new_weather = random.choice([Weather.LIGHT_RAIN, Weather.HEAVY_RAIN])
+        if random.random() >= 0.30:
+            return None, None
 
-            elif weather == Weather.LIGHT_RAIN:
-                new_weather = random.choice([Weather.DRY, Weather.HEAVY_RAIN])
+        change_lap = random.randint(15, 45)
 
-            else:  
-                new_weather = Weather.LIGHT_RAIN
+        if weather == Weather.DRY:
+            new_weather = random.choice([Weather.LIGHT_RAIN, Weather.HEAVY_RAIN])
+
+        elif weather == Weather.LIGHT_RAIN:
+            new_weather = random.choice([Weather.DRY, Weather.HEAVY_RAIN])
+
+        else:
+            new_weather = Weather.LIGHT_RAIN
+
+        return change_lap, new_weather
+
+    def generate(self):
+        weather = self.generate_weather()
+        safety_car_lap = self.generate_safety_car()
+        weather_change_lap, new_weather = self.generate_weather_change(weather)
 
         return RaceEnvironment(weather=weather,
             safety_car_lap=safety_car_lap,
             weather_change_lap=weather_change_lap,
-            new_weather=new_weather,)
+            new_weather=new_weather)
