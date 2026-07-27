91336e046919be6a1dee69cb3479a97cb26ff40b
KashanAli-Tech <off.kashanali@gmail.com>
2026-07-27 13:12:17 +0100

    Improved tyre degradation model with tyre cliff behaviour

--- src/models/tyre.py
+++ src/models/tyre.py
@@ -11,15 +11,16 @@ class Tyre:
     degradation_rate: float
     cliff_lap: int
 
-    def calculate_degradation(self, tyre_age: int, tyre_management: float) -> float:
+    def calculate_degradation(self, tyre_age, driver_management):
+        base_wear = self.degradation_rate * tyre_age
 
-        effective_rate = (self.degradation_rate * (2 - tyre_management))
-        base_degradation = tyre_age * effective_rate
-
+        # driver tyre management reduces wear
+        management_factor = 1 - (driver_management * 0.2)
+        degradation = base_wear * management_factor
+        
+        # tyre cliff
         if tyre_age > self.cliff_lap:
-            base_degradation += ((tyre_age - self.cliff_lap) * self.degradation_rate)
+            cliff_factor = (tyre_age - self.cliff_lap) ** 1.5
+            degradation += cliff_factor * 0.05
 
-        # the uncertainty in tyre behaviour
-        degradation_variation = random.normalvariate(0, 0.05)
-        degradation = base_degradation + degradation_variation
-        return max(degradation, 0)
+        return degradation
