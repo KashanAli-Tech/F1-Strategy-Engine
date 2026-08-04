5aaa0630e09023c6ed1c14842e203ec7d02e4256
KashanAli-Tech <off.kashanali@gmail.com>
2026-08-04 22:29:34 +0100

    Added streamlit dashboard foundation and navigation structure

---
+++ dashboard/pages/calibration.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Track Calibration")
+    st.info("Track model parameters will be displayed here.")
---
+++ dashboard/pages/strategy_optimiser.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+def show():
+    st.title("Strategy Optimiser")
+    st.info("Strategy optimisation results will be displayed here.")
